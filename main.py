92587a1b15f7b6c4bc45d6e7d30f3ba4a82565dd
srkds <im.nirav86@gmail.com>
2024-05-19 07:14:52 +0530

    train model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils import load_json
 
 config = load_json("./config/config.json")
 
-runner = Runner()
+runner = Runner(config, [1,2,3], 4) # train video list, test video
 
 runner.train()
-runner.output()
+# runner.output()
